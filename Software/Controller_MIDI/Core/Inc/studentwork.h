563ec17dc878672a10b26752623b0fa4c3ad54c4
Equipe <nicolas.haas@netc.fr>
2024-03-25 17:35:08 +0100

    software STM32

---
+++ Software/Controller_MIDI/Core/Inc/studentwork.h
@@ -0,0 +1,16 @@
+/*
+ * studentwork.h
+ *
+ *  Created on: Mar 25, 2024
+ *      Author: eleve
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef INC_STUDENTWORK_H_
+#define INC_STUDENTWORK_H_
+#endif /* INC_STUDENTWORK_H_ */
+
+void setup(void);
+void loop(void);
